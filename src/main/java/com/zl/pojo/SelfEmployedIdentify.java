aad4d6042925d1137b67ae3a921f1cca05aa945f
huang <292449967@qq.com>
2019-07-12 20:26:12 +0800

    加了备注的POJO表

--- src/main/java/com/zl/pojo/SelfEmployedIdentify.java
+++ src/main/java/com/zl/pojo/SelfEmployedIdentify.java
@@ -2,24 +2,31 @@ package com.zl.pojo;
 
 import java.util.Date;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Component;
 
+/**
+ * 个人认证信息表
+ * */
+@Component
+@Scope("prototype")
 public class SelfEmployedIdentify {
-	private Integer id;
-	private String identifyNo;
-	private String realName;
-	private String idNo;
-	private String idPhoto;
-	private Integer provinceId;
-	private Integer cityId;
-	private Integer districtId;
-	private String mobileNo;
-	private String email;
-	private String phoneNo;
-	@DateTimeFormat(pattern="yyyy-MM-dd")
-	private Date submissionDate;
-	private Integer state ;
-	private Integer userId;
+	private Integer id;         //主键ID，自增
+	private String identifyNo;    //认证表No
+	private String realName;      //真实姓名
+	private String idNo;    //身份证号码
+	private String idPhoto;      //身份证照片（存照片地址）
+	private Integer provinceId;       //省ID，外键，连接省表
+	private Integer cityId;     //市ID，外键，连接市表
+	private Integer districtId;      //区ID，外键，连接区表
+	private String mobileNo;     //手机号码
+	private String email;       //电子邮箱
+	private String phoneNo;      //单位座机
+	@DateTimeFormat(pattern="yyyy-MM-dd HH-mm-ss")
+	private Date submissionDate;      //上传日期
+	private Integer state ;        //状态  0未通过 1待审核 2通过
+	private Integer userId;      //用户id 外键连接User表
 	public Integer getId() {
 		return id;
 	}
