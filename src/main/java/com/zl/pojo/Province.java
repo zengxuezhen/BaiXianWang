29a611a43f7d303618bd0dbf0313a37d4efa7674
Username <Username@example.com>
2019-07-12 20:33:04 +0800

    pojo

---
+++ src/main/java/com/zl/pojo/Province.java
@@ -0,0 +1,34 @@
+package com.zl.pojo;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+/*
+ * 省表
+ * */
+public class Province {
+	private Integer id;//主键ID，自增
+	private String provinceNo;//省
+	private String provinceName;//省名
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getProvinceNo() {
+		return provinceNo;
+	}
+	public void setProvinceNo(String provinceNo) {
+		this.provinceNo = provinceNo;
+	}
+	public String getProvinceName() {
+		return provinceName;
+	}
+	public void setProvinceName(String provinceName) {
+		this.provinceName = provinceName;
+	}
+
+}
